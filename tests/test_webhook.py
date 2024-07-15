faa368f9e5c35cf615a55da312e59e4d2664bc74
Frederik Rajiv Manichand <rajivmanichand@gmail.com>
2024-07-15 11:48:11 +0200

    Tests

---
+++ tests/test_webhook.py
@@ -0,0 +1,81 @@
+from waapi_wrapper.webhook import handle_waapi_event
+from waapi_wrapper.events import MessageEvent, DisconnectedEvent, GroupJoinEvent
+import json
+
+
+def test_handle_message_event():
+    raw_data_message = """
+    {
+        "event": "message",
+        "instanceId": "33",
+        "data": {
+            "message": {
+                "id": {
+                    "fromMe": false,
+                    "remote": "50611223344@c.us",
+                    "id": "V6B05T14AAIWWPEL7MJC",
+                    "_serialized": "false_50611223344@c.us_V6B05T14AAIWWPEL7MJC"
+                },
+                "body": "Hey. I am fine. Thanks. Your test message was successful ;)",
+                "type": "chat",
+                "timestamp": 1669994807,
+                "from": "50611223344@c.us",
+                "to": "50611223355@c.us",
+                "notifyName": "John"
+            }
+        }
+    }
+    """
+    event = handle_waapi_event(json.loads(raw_data_message))
+    assert isinstance(event, MessageEvent)
+    assert event.instance_id == "33"
+    assert event.notifyName == "John"
+    assert (
+        event.message_body == "Hey. I am fine. Thanks. Your test message was successful ;)"
+    )
+    assert event.from_ == "50611223344@c.us"
+    assert event.to == "50611223355@c.us"
+
+
+def test_disconnected_event():
+    raw_data_disconnected = """
+    {
+        "event": "disconnected",
+        "instanceId": "12345",
+        "data": {}
+    }
+    """
+    event = handle_waapi_event(json.loads(raw_data_disconnected))
+    assert isinstance(event, DisconnectedEvent)
+    assert event.instance_id == "12345"
+
+
+def test_group_join_event():
+    raw_data_group_join = """
+    {
+        "event": "group_join",
+        "instanceId": "12345",
+        "data": {
+            "notification": {
+                "id": {
+                    "fromMe": false,
+                    "remote": "120363279485465785@g.us",
+                    "id": "21781889331718643453",
+                    "participant": "3197010267647@c.us"
+                },
+                "body": null,
+                "type": "add",
+                "timestamp": 1718643453,
+                "chatId": "120363279485465785@g.us",
+                "author": "31633767338@c.us",
+                "recipientIds": [
+                    "3197010267647@c.us"
+                ]
+            }
+        }
+    }
+    """
+    event = handle_waapi_event(json.loads(raw_data_group_join))
+    assert isinstance(event, GroupJoinEvent)
+    assert event.instance_id == "12345"
+    assert event.author == "31633767338@c.us"
