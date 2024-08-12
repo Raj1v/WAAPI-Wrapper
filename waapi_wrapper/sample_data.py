ea32afb2a7cd6cd7b4656befdf3583961a95b3de
Frederik Rajiv Manichand <rajivmanichand@gmail.com>
2024-08-12 18:28:44 +0200

    Include Sample Data

---
+++ waapi_wrapper/sample_data.py
@@ -0,0 +1,45 @@
+from waapi_wrapper.webhook import handle_waapi_event
+from waapi_wrapper.events import (
+    TextMessageEvent,
+    VoiceMessageEvent,
+)
+import json
+import importlib.resources as pkg_resources
+
+
+def sample_text_message_event() -> TextMessageEvent:
+    raw_data_message = """
+    {
+        "event": "message",
+        "instanceId": "33",
+        "data": {
+            "message": {
+                "id": {
+                    "fromMe": false,
+                    "remote": "50611223344@c.us",
+                    "id": "V6B05T14AAIWWPEL7MJC",
+                    "_serialized": "false_50611223344@c.us_V6B05T14AAIWWPEL7MJC"
+                },
+                "body": "Hey. I am fine. Thanks. Your test message was successful ;)",
+                "type": "chat",
+                "timestamp": 1669994807,
+                "from": "50611223344@c.us",
+                "to": "50611223355@c.us",
+                "notifyName": "John"
+            }
+        }
+    }
+    """
+    event = handle_waapi_event(json.loads(raw_data_message))
+    if not isinstance(event, TextMessageEvent):
+        raise ValueError("Event is not an instance of TextMessageEvent")
+    return event
+
+
+def sample_voice_message_event() -> VoiceMessageEvent:
+    with pkg_resources.path("waapi_wrapper", "test/data/audio_message.json") as path:
+        raw_data_voice_message = json.load(open(path))
+
+    event = handle_waapi_event(raw_data_voice_message)
+    assert isinstance(event, VoiceMessageEvent)
+    return event
